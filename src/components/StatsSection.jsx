f6c48a639b37dabfeb55a435cd455f8a136dc9e0
Stephen Kettley <kettleystephen@gmail.com>
2024-02-13 18:33:24 +0200

    add stats section

---
+++ src/components/StatsSection.jsx
@@ -0,0 +1,15 @@
+const StatsSection = () => {
+  return (
+    <div className="py-24 bg-gray-100">
+      <h2 className="text-4xl mb-6 font-bold text-center">
+        Advanced Statistics
+      </h2>
+      <p className="max-w-xs mx-auto text-center text-gray-400 md:max-w-m">
+        Track how your links are performing across the web with our advanced
+        statistics dashboard.
+      </p>
+    </div>
+  );
+};
+
+export default StatsSection;
