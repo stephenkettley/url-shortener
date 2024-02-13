90cca489b3ab8bc8685e81fd99def25980da55df
Stephen Kettley <kettleystephen@gmail.com>
2024-02-13 18:33:32 +0200

    update styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
+import CTASection from "./components/CTASection";
+import FeatureBoxSection from "./components/FeatureBoxSection";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import ShortenSection from "./components/ShortenSection";
+import StatsSection from "./components/StatsSection";
 
 function App() {
   return (
@@ -9,6 +13,10 @@ function App() {
       <Navbar></Navbar>
       <Hero></Hero>
       <ShortenSection></ShortenSection>
+      <StatsSection></StatsSection>
+      <FeatureBoxSection></FeatureBoxSection>
+      <CTASection></CTASection>
+      <Footer></Footer>
     </>
   );
 }
