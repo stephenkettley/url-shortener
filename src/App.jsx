cc30477b9b855f36625a1960964d284e428b634e
Stephen Kettley <kettleystephen@gmail.com>
2024-02-13 15:43:29 +0200

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import ShortenSection from "./components/ShortenSection";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
       <Hero></Hero>
+      <ShortenSection></ShortenSection>
     </>
   );
 }
