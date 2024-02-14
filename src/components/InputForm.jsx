409a1cf40b010363f9cf70afa8985883da7c5fc3
Stephen Kettley <kettleystephen@gmail.com>
2024-02-14 10:59:12 +0200

    add react hook form validation

--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,34 +1,47 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const InputForm = ({ onSubmit }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm();
   const [input, setInput] = useState("");
 
-  function submitForm(event) {
-    event.preventDefault();
-    setInput("");
-    onSubmit(input);
+  function submitForm(data) {
+    onSubmit(data.link);
+    reset();
   }
   return (
-    <form
-      className="relative flex flex-col w-full p-10 -mt-20 space-y-4 bg-darkViolet rounded-lg md:flex-row md:space-y-0 md:space-x-3"
-      onSubmit={submitForm}
-    >
-      <input
-        type="url"
-        className="flex-1 p-3 border-2 rounded-lg placeholder-yellow-500 focus:outline-none"
-        placeholder="Shorten a link here"
-        id="link-input"
-        value={input}
-        onChange={(event) => setInput(event.target.value)}
-      />
-      <button
-        className="px-10 py-3 text-white bg-cyan rounded-lg hover:bg-cyanLight focus:outline-none md:py-2"
-        type="submit"
+    <div>
+      <form
+        className="relative flex flex-col w-full p-10 py-12 -mt-20 space-y-4 bg-darkViolet rounded-lg md:flex-row md:space-y-0 md:space-x-3"
+        onSubmit={handleSubmit(submitForm)}
       >
-        Shorten It!
-      </button>
-      <div className="absolute left-7 bottom-3 text-red text-sm italic"></div>
-    </form>
+        <input
+          {...register("link", {
+            required: "Link is required!",
+          })}
+          type="url"
+          className="flex-1 p-3 border-2 rounded-lg placeholder-yellow-500 focus:outline-none"
+          placeholder="Shorten a link here"
+          id="link-input"
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        />
+        <button
+          className="px-10 py-3 text-white bg-cyan rounded-lg hover:bg-cyanLight focus:outline-none md:py-2"
+          type="submit"
+        >
+          Shorten It!
+        </button>
+        <div className="absolute left-7 bottom-3 text-red text-md">
+          {errors.link && <p>{`*${errors.link.message}`}</p>}
+        </div>
+      </form>
+    </div>
   );
 };
 
